2bf121d4f83616480ebb447d676832a71502ceeb
ImThePacific <prashant997341@gmail.com>
2024-09-03 18:46:41 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY DELTAxKING
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7539179971:AAFOiQM1aXvssQWeZtZmvOE_eAL4ftjtz60")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("ImThePacific")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7243326554")
+
+
+
+
+
+
+
+  
